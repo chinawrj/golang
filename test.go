671fde6801324d0c02416d3d5219ab6169f2b1f2
Rujun Wang <chinawrj@gmail.com>
2017-03-26 17:08:41 +0800

    test array

--- test.go
+++ test.go
@@ -29,10 +29,16 @@ func animal_speak(a animal) {
     a.speak();
 }
 
+func test_array () {
+    var a = []int {1, 2, 3, 4, 5};
+    log.Println("array is ", a);
+}
+
 func main() {
     p1 := person{"GS"};
     c1 := cat{"Kitty "};
     animal_speak(p1);
     animal_speak(c1);
+    test_array();
     log.Println(http.ListenAndServe("0.0.0.0:6060", nil))
 }
